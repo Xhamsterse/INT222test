1f2f8ebf459e98e1e5b047c11d3112530a492033
Xhamsterse <krit.bank@mail.kmutt.ac.th>
2021-09-23 19:51:27 +0700

    test1

---
+++ pages/Accommodation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+    <Card />
+    <Pricing />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+
+
+
+export default Vue.extend({})
+</script>
---
+++ pages/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Navbar />
+    <Progressbar />
+    <Ecommerce />
+    <Form />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
